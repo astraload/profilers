1bcf75bac61b68de5744e10c0697d9ab82326e3a
baryshok <baryshok@gmail.com>
2019-11-21 20:01:41 +0300

    Export logInColor function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { Profilers } = require('./src/profilers');
 const { TaskEvent } = require('./src/constants');
+const { logInColor } = require('./src/helpers');
 
 module.exports = {
   Profilers,
   TaskEvent,
+  logInColor,
 };
