df2cadd6b23c584b098e4a371afbf72b64b32233
baryshok <baryshok@gmail.com>
2019-11-22 01:04:31 +0300

    Add generateId helper method and shorten id to 12 chars

--- src/helpers.js
+++ src/helpers.js
@@ -2,4 +2,12 @@ function logInColor(message) {
   console.log('\x1b[36m%s\x1b[0m', message);
 }
 
-module.exports = { logInColor };
+function generateId() {
+  const idLength = 12;
+  return new Mongo.ObjectID().valueOf().slice(0, idLength);
+}
+
+module.exports = {
+  logInColor,
+  generateId,
+};
