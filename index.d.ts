8cb04e195c7948177de5b36fcf462b5b8cf18f30
baryshok <baryshok@gmail.com>
2021-07-07 01:09:39 +0300

    Add typescript declaration file

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+import { EventEmitter } from 'events';
+
+export class CpuProfiler {
+  isProfiling(): boolean;
+  setProfiling(value: boolean): void;
+
+  profile(
+    duration: number,
+    samplingInterval: number
+  ): Promise<{ fileName: string; filePath: string }>;
+
+  scheduleTask(
+    instanceName: string,
+    duration: number,
+    samplingInterval: number
+  ): void;
+}
+
+export class HeapDumper {
+  isDumping(): boolean;
+  setDumping(value: boolean): void;
+  takeSnapshot(): { fileName: string; filePath: string };
+  scheduleTask(instanceName: string): void;
+}
+
+export class Profilers extends EventEmitter {
+  cpu: CpuProfiler;
+  heap: HeapDumper;
+  startObserving(instanceName: string): void;
+  stopObserving(): void;
+}
+
+export enum TaskEvent {
+  CpuProfileCreated = 'cpuProfileCreated',
+  HeapSnapshotCreated = 'heapSnapshotCreated',
+}
+
+export function logInColor(message: string): void;
+export const CpuProfileFileExt: 'cpuprofile';
+export const HeapSnapshotFileExt: 'heapsnapshot';
